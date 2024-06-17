814991207a32c1a554cd8b656c18f254a00fe20f
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2024-06-17 19:12:08 -0400

    Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module password.password.password {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens password.password.password to javafx.fxml;
+    exports password.password.password;
+}
